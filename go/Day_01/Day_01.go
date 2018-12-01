c5787e14fa443aa1c866a484c67e9b454975ba09
Jell <jean-louis@jawaninja.com>
2018-12-01 14:56:54 +0100

    Try to make go work

---
+++ go/Day_01/Day_01.go
@@ -0,0 +1,11 @@
+package Day_01
+
+import("fmt")
+
+func Part_1() {
+	fmt.Printf("Day 01 - Part 01\n");
+}
+
+func Part_2() {
+	fmt.Printf("Day 01 - Part 02\n");
+}
---
+++ go/main.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+	"advent2018/Day_01"
+)
+
+func main() {
+	Day_01.Part_1();
+	Day_01.Part_2();
+}
