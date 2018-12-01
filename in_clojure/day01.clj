45783a4157ef65cfe17374de9115b9e6a795b2de
Jell <jean-louis@jawaninja.com>
2018-12-01 20:12:50 +0100

    Day 1 - clojure

--- in_clojure/day01.clj
+++ in_clojure/day01.clj
@@ -1,2 +1,22 @@
-(println "Day 1 - Part 1")
-(println "Day 1 - Part 2")
+(ns day01
+  (:require [clojure.string :as str]))
+
+(def input
+  (->> "../inputs/day01.txt"
+       (slurp)
+       (str/split-lines)
+       (map read-string)))
+
+(->> input
+     (reduce +)
+     (println "Day 1 - Part 1: "))
+
+(->> input
+     (cycle)
+     (reductions +)
+     (reduce (fn [seen new]
+               (if (contains? seen new)
+                 (reduced new)
+                 (conj seen new)))
+             #{})
+     (println "Day 1 - Part 2: "))
