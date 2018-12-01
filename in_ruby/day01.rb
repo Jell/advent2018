5e3d94a3df879a1bb0d529c2f61f078744cb6bce
Jell <jean-louis@jawaninja.com>
2018-12-01 20:52:36 +0100

    Day 1 - Ruby

--- in_ruby/day01.rb
+++ in_ruby/day01.rb
@@ -1,2 +1,14 @@
-puts "Day 1 - part 1"
-puts "Day 1 - part 1"
+require 'set'
+
+input = File.read("../inputs/day01.txt").lines.map(&:to_i)
+
+part1 = input.reduce(&:+)
+
+puts "Day 1 - part 1: #{part1}"
+
+part2 = input.cycle.reduce([0, Set.new]) do |(freq, seen), i|
+  freq += i
+  seen.include?(freq) ? (break freq) : [freq, seen << freq]
+end
+
+puts "Day 1 - part 2: #{part2}"
