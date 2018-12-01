f6e92ab371ac230bcc3496db1d49931c48f4998a
Jell <jean-louis@jawaninja.com>
2018-12-01 15:45:45 +0100

    Try something else

---
+++ go/Day_01/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Part_1() {
+	fmt.Printf("Day 01 - Part 01!\n")
+}
+
+func Part_2() {
+	fmt.Printf("Day 01 - Part 02!\n")
+}
+
+func main() {
+	Part_1()
+	Part_2()
+}
