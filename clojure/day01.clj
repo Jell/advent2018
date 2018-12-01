f4a5f40205aa82a585f2f281023218b00e955f73
Jell <jean-louis@jawaninja.com>
2018-12-01 19:05:12 +0100

    Add clojure

---
+++ clojure/day01.clj
@@ -0,0 +1,10 @@
+(ns day01)
+
+(defn part-1 []
+  (println "Day 1 - Part 1"))
+
+(defn part-2 []
+  (println "Day 1 - Part 2"))
+
+(part-1)
+(part-2)
---
+++ clojure/day02.clj
@@ -0,0 +1,10 @@
+(ns day02)
+
+(defn part-1 []
+  (println "Day 2 - Part 1"))
+
+(defn part-2 []
+  (println "Day 2 - Part 2"))
+
+(part-1)
+(part-2)
