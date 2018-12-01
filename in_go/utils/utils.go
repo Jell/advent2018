49713abf5e6dba273b535f137ade72ce64837f24
Jell <jean-louis@jawaninja.com>
2018-12-01 21:33:48 +0100

    More utils

--- in_go/utils/utils.go
+++ in_go/utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
-import ()
+import (
+	"io/ioutil"
+	"strings"
+)
 
 // Check ...
 // panic if error
@@ -8,4 +11,17 @@ func Check(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+// ReadFile ...
+func ReadFile(path string) string {
+	raw, err := ioutil.ReadFile("../../inputs/day01.txt")
+	Check(err)
+	return string(raw)
+}
+
+// ReadLines ...
+func ReadLines(path string) []string {
+	var lines = strings.Split(ReadFile(path), "\n")
+	return lines[:len(lines)-1]
 }
