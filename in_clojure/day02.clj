95cc79d177243e64bf04c56ce85df79dea1421c0
Jell <jean-louis@jawaninja.com>
2018-12-02 20:22:57 +0100

    Day 2 Clojure

--- in_clojure/day02.clj
+++ in_clojure/day02.clj
@@ -1,2 +1,62 @@
-(println "Day 2 - Part 1")
-(println "Day 2 - Part 2")
+(ns day02
+  (:require [clojure.string :as str]
+            [clojure.set :refer [map-invert]]))
+
+(def input
+  (->> "../inputs/day02.txt"
+       (slurp)
+       (str/split-lines)))
+
+(defn row-checksum [row]
+  (->> row
+       (sort)
+       (partition-by identity)
+       (map count)
+       (set)))
+
+(defn flip-select-keys [ks m]
+  (select-keys m ks))
+
+(->> input
+     (mapcat row-checksum)
+     (group-by identity)
+     (map (fn [[k v]] [k (count v)]))
+     (into {})
+     (flip-select-keys #{2 3})
+     (vals)
+     (apply *)
+     (println "Day 2 - Part 1: "))
+
+(defn one-less-options [row]
+  (let [c (count row)]
+    (vec
+     (for [i (range c)]
+       (str (subs row 0 i)
+            (subs row (inc i) c))))))
+
+(defn transpose [m]
+  (let [rows-count (count m)
+        columns-count (count (first m))]
+    (vec
+     (for [j (range columns-count)]
+       (vec
+        (for [i (range rows-count)]
+          (get-in m [i j])))))))
+
+(defn flip-find [e c]
+  (find c e))
+
+(defn paired-match [rows]
+  (->> rows
+       (group-by identity)
+       (map (fn [[k v]] [k (count v)]))
+       (into {})
+       (map-invert)
+       (flip-find 2)
+       second))
+
+(->> input
+     (mapv one-less-options)
+     (transpose)
+     (some paired-match)
+     (println "Day 2 - Part 2: "))
