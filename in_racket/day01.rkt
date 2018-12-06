b092231f4b52935567974951fdbb994c3249c2a0
Jell <jean-louis@jawaninja.com>
2018-12-06 21:59:41 +0100

    Day 1 part 2 racket with call/cc

--- in_racket/day01.rkt
+++ in_racket/day01.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (require threading)
+(require racket/set)
 
 (define input
   (~>> "../inputs/day01.txt"
@@ -10,3 +11,16 @@
   (foldl + 0 input))
 
 (displayln (format "Day 01 - Part 1: ~a" part-1))
+
+(define part-2
+  (call/cc
+   (λ (return)
+     (let loop ([rows input] [seen (set)] [ freq 0 ])
+       (if (empty? rows)
+           (loop input seen freq)
+           (let ([freq (+ (car rows) freq)])
+             (if (set-member? seen freq)
+                 (return freq)
+                 (loop (cdr rows) (set-add seen freq) freq))))))))
+
+(displayln (format "Day 01 - Part 2: ~a" part-2))
