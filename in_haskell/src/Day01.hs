76f3d74073b8b32c03fb335b0628046a47269722
Jell <jean-louis@jawaninja.com>
2018-12-06 21:15:58 +0100

    Day 1 in Haskell

--- in_haskell/src/Day01.hs
+++ in_haskell/src/Day01.hs
@@ -1,7 +1,33 @@
 module Day01 (part1, part2) where
+import qualified Data.Set
+
+parseInt :: String -> Int
+parseInt x = read $ filter (`elem` "-0123456789") x
+
+loadInput :: IO ([Int])
+loadInput = do
+  raw <- readFile "../inputs/day01.txt"
+  return $ map parseInt $ lines raw
 
 part1 :: IO ()
-part1 = putStrLn "Day 01 - Part 1"
+part1 = do
+  input <- loadInput
+  putStr "Day 01 - Part 1: "
+  putStrLn $ show $ foldl (+) 0 input
+
+subsolve2 :: [Int] -> [Int] -> Data.Set.Set Int -> Int -> Int
+subsolve2 origin [] seen freq = subsolve2 origin origin seen freq
+subsolve2 origin (i : is) seen freq =
+  let freq' = i + freq
+  in if Data.Set.member freq' seen
+     then freq'
+     else subsolve2 origin is (Data.Set.insert freq' seen) freq'
+
+solve2 :: [Int] -> Int
+solve2 input = subsolve2 input [] Data.Set.empty 0
 
 part2 :: IO ()
-part2 = putStrLn "Day 01 - Part 2"
+part2 = do
+  input <- loadInput
+  putStr "Day 01 - Part 2: "
+  putStrLn $ show $ solve2 input
