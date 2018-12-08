8a5ff618b9287009107d3c5509c2a42d8c921c95
Jell <jean-louis@jawaninja.com>
2018-12-08 21:15:54 +0100

    Faster clojure Day 5

--- in_clojure/day05.clj
+++ in_clojure/day05.clj
@@ -1,39 +1,40 @@
 (ns day05
   (:require [clojure.string :as str]))
 
-(def input "dabAcCaCBAcCcaDA")
 (def input (str/trim (slurp "../inputs/day05.txt")))
 
 (def alphabet "abcdefghijklmnopqrstuvwxyz")
 (def ALPHABET "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-(def reaction-regex
-  (->> (map str alphabet ALPHABET)
-       (concat (map str ALPHABET alphabet))
-       (str/join "|")
-       re-pattern))
+(def reactive
+  (zipmap (str alphabet ALPHABET)
+          (str ALPHABET alphabet)))
 
-(defn react [polymer]
-  (loop [x ^String polymer]
-    (let [x2 (str/replace x reaction-regex "")]
-      (if (= (.length x2) (.length x))
-        x
-        (recur x2)))))
+(defn react [input]
+  (if-let [c1 (first input)]
+    (if-let [next-step (react (rest input))]
+      (if-let [c2 (first next-step)]
+        (if (= c2 (reactive c1))
+          (rest next-step)
+          (cons c1 next-step))
+        (cons c1 next-step))
+      (list c1))
+    (list)))
 
 (def initial-reaction (react input))
 
-(def part-1 (.length initial-reaction))
+(def part-1 (count initial-reaction))
 
 (println "Day 05 - Part 1:" part-1)
 
 (def part-2
   (->> alphabet
        (pmap (fn [c]
-               (-> initial-reaction
-                   (str/replace (str/lower-case c) "")
-                   (str/replace (str/upper-case c) "")
-                   react
-                   count)))
+               (->> initial-reaction
+                    (remove #{(first (str/lower-case c))
+                              (first (str/upper-case c))})
+                    react
+                    count)))
        (apply min)))
 
 (println "Day 05 - Part 2:" part-2)
